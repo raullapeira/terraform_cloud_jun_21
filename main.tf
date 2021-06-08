1d8e11010d96ce1c0194dd0d8b88ffb0682e5b8b
vn.rlherrero <raul.lapeira@gmail.com>
2021-06-08 16:11:31 +0200

    26 commit

--- main.tf
+++ main.tf
@@ -7,15 +7,13 @@ terraform {
   }
 }
 
-variable "client_secret" {
-}
 
 provider "azurerm" {
   features {}
 
   subscription_id = "a16aaf45-027e-4133-91fb-18dbdce17944"
   client_id       = "NDV2_A~y-9ubEiG-wiA12HA06rv_q08JXx"
-  client_secret   = var.client_secret
+  client_secret   = "83bdf1fe-42b3-4944-89ad-62fa4a9009f8"
   tenant_id       = "c90134c8-2744-41e7-b3ca-e8656f25725a"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
 variable "client_secret" {
-  type    = number
   default = "83bdf1fe-42b3-4944-89ad-62fa4a9009f8"
 }
