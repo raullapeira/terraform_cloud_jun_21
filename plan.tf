ce5d8129d7089b9095348caf9eeebe6173fa89be
vn.rlherrero <raul.lapeira@gmail.com>
2021-06-08 14:54:39 +0200

    4 commit

--- plan.tf
+++ plan.tf
@@ -1,8 +1,12 @@
+provider "azurerm" {
+	features {}
+}
+
 module "plan_basico" {
-  source  = "./plan_basico"
+	source  = "./plan_basico"
 }
 module "azure-region" {
-  source  = "claranet/regions/azurerm"
-  version = "4.0.0"
-  azure_region = "eu-west"
+	source  = "claranet/regions/azurerm"
+	version = "4.0.0"
+	azure_region = "eu-west"
 }
