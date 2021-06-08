516d4b9ed646808dd692dd61786ea7ff87d33691
vn.rlherrero <raul.lapeira@gmail.com>
2021-06-08 16:12:45 +0200

    26 commit

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = "a16aaf45-027e-4133-91fb-18dbdce17944"
-  client_id       = "NDV2_A~y-9ubEiG-wiA12HA06rv_q08JXx"
+  client_id       = "d4a7e044-bc8a-4f1e-9b14-ef59938bebd1"
   client_secret   = "83bdf1fe-42b3-4944-89ad-62fa4a9009f8"
   tenant_id       = "c90134c8-2744-41e7-b3ca-e8656f25725a"
 }
