bd091654bdf1aaf86cda91ea4c0c4c64a3704555
vn.rlherrero <raul.lapeira@gmail.com>
2021-06-08 17:58:51 +0200

    commit 28

--- main.tf
+++ main.tf
@@ -7,19 +7,9 @@ terraform {
   }
 }
 
-
-provider "azurerm" {
-  features {}
-
-  subscription_id = "a16aaf45-027e-4133-91fb-18dbdce17944"
-  client_id       = "d4a7e044-bc8a-4f1e-9b14-ef59938bebd1"
-  client_secret   = "NDV2_A~y-9ubEiG-wiA12HA06rv_q08JXx"
-  tenant_id       = "c90134c8-2744-41e7-b3ca-e8656f25725a"
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources2"
   location = "West Europe"
 }
 
