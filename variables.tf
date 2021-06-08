55e4512d9d5ad8533ddbaad3d055a99a44509eec
vn.rlherrero <raul.lapeira@gmail.com>
2021-06-08 16:03:48 +0200

    24 commit

--- variables.tf
+++ variables.tf
@@ -1,19 +1,4 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
+variable "client_secret" {
   type    = number
   default = 1
 }
